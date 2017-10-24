a0f1d4cd4f9785a2aef05003cb6bc5847465a9a7
Ryan Petersburg <rpetersburg@gmail.com>
2017-10-24 18:47:33 -0400

    functions and adding data to repo

--- transit_analysis.py
+++ transit_analysis.py
@@ -3,10 +3,14 @@ import numpy as np
 
 data_dict = {}
 
-with open('surveyResults_AllGPSS.csv', 'rb') as f:
-    data = np.asarray(csv.reader(f))
+def get_data():
+    with open('surveyResults_AllGPSS.csv', 'rt') as f:
+        data = np.asarray(list(csv.reader(f)))
+    return data
+    
+def analyze_data():
     for i in xrange(len(data[0])):
         column = data[:,i]
         data_dict[column[0]] = column[2:]
-        print column
+        print(column)
     
